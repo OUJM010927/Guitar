da00073cfd810cbd5f38690f7c6b8b8a8ea7688e
OUJM010927 <22670367@itiguala.edu.mx>
2023-03-17 22:30:45 -0600

    Se agrega el Proyecto

---
+++ src/guitar/Guitar.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guitar;
+
+/**
+ *
+ * @author Michelle
+ */
+public class Guitar {
+
+    /**
+     * @param args the command line arguments
+     */
+    private String serialNumber;
+
+    private double price;
+
+
+    private String builder;
+
+    private String model;
+
+
+    private String type;
+
+    private String backWood;
+
+    private String topWood;
+
+}
